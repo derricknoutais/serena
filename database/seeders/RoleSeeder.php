278eb6b1954d10a2df536561d06beeb124480bb8
derricknoutais <derricknoutais@gmail.com>
2025-11-27 22:48:15 +0100

    Switch from Permission to Role Based

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\PermissionRegistrar;
 
 class RoleSeeder extends Seeder
 {
@@ -14,58 +12,21 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        app(PermissionRegistrar::class)->forgetCachedPermissions();
-
         $guardName = config('auth.defaults.guard', 'web');
 
-        $permissions = [
-            'manage_hotel',
-            'manage_staff',
-            'manage_bookings',
-            'manage_housekeeping',
-            'manage_finances',
-            'view_reports',
-        ];
-
-        foreach ($permissions as $permission) {
-            Permission::query()->firstOrCreate(
-                [
-                    'name' => $permission,
-                    'guard_name' => $guardName,
-                ],
-            );
-        }
-
-        $rolePermissions = [
-            'owner' => $permissions,
-            'manager' => [
-                'manage_hotel',
-                'manage_staff',
-                'manage_bookings',
-                'manage_housekeeping',
-                'view_reports',
-            ],
-            'receptionist' => [
-                'manage_bookings',
-            ],
-            'housekeeping' => [
-                'manage_housekeeping',
-            ],
-            'accountant' => [
-                'manage_finances',
-                'view_reports',
-            ],
-        ];
-
-        foreach ($rolePermissions as $role => $permissionSet) {
-            $roleModel = Role::query()->firstOrCreate(
+        foreach ([
+            'owner',
+            'manager',
+            'receptionist',
+            'housekeeping',
+            'accountant',
+        ] as $role) {
+            Role::query()->firstOrCreate(
                 [
                     'name' => $role,
                     'guard_name' => $guardName,
                 ],
             );
-
-            $roleModel->syncPermissions($permissionSet);
         }
     }
 }
